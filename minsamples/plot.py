5a14b1c9f511abf0cce8b01def881802965cf8e5
Italo Cunha <cunha@dcc.ufmg.br>
2019-04-23 21:18:15 +0000

    add summary minsamples graph

--- minsamples/plot.py
+++ minsamples/plot.py
@@ -33,7 +33,7 @@ def create_parser():
             nargs='+',
             metavar='NUMBERS',
             required=False,
-            default=[50, 100, 150, 200, 250, 500, 750, 1000],
+            default=[0, 50, 100, 150, 200, 250, 500, 750, 1000],
             help='Limits on the number of samples %(default)s')
     return parser
 
@@ -67,6 +67,19 @@ def read_cdf(fpath):
     return cdf
 
 
+def cdf_get_inv_percentiles(cdf, percentiles):
+    invp = list()
+    i = 1
+    for p in percentiles:
+        if p >= 1.0:
+            raise ValueError('percentiles must be smaller than 1.0')
+        while cdf[i][1] < p:
+            i += 1
+        invp.append(cdf[i-1][0])
+    assert len(invp) == len(percentiles)
+    return invp
+
+
 def plot_ci_size_cdfs(limit2traffic, limit2cdf, xlim, xlabel, outfn):
     fig, ax1 = plt.subplots()
     ax1.set_xlabel(xlabel, fontsize=16)
@@ -80,6 +93,31 @@ def plot_ci_size_cdfs(limit2traffic, limit2cdf, xlim, xlabel, outfn):
         ax1.plot(xs, ys, label=label)
     plt.legend(loc='best')
     plt.grid()
+    plt.savefig(outfn, bbox_inches='tight')
+    plt.close(fig)
+
+
+def plot_limits(limit2traffic, limit2cdf, pct, outfn):
+    fig, ax1 = plt.subplots()
+    ax2 = ax1.twinx()
+    ax1.set_xlim(0, 1000)
+    ax1.set_xlabel("Minimum Number of Samples", fontsize=16)
+    ax1.set_ylabel("P90 of P%d MinRTT CI Size [ms]" % pct, fontsize=16)
+    ax1.set_ylim(0, 50)
+    ax2.set_ylabel("Fraction of Traffic", fontsize=16)
+    ax2.set_ylim(0.5, 1)
+    fig.tight_layout()
+    xs, cdfs = zip(*list(sorted(limit2cdf.items())))
+    qtiles = list(tuple(cdf_get_inv_percentiles(cdf, [0.50, 0.90]))
+            for cdf in cdfs)
+    _p50s, p90s = zip(*qtiles)
+    # ax1.fill_between(xs, p10s, p90s, linewidth=0, linestyle=None, color="#333333", alpha=0.25)
+    ax1.plot(xs, p90s, 'ko-', linewidth=2, markersize=5, label="CI Size")
+    ax1.legend(loc='lower left')
+    xs, ys = zip(*list(sorted(limit2traffic.items())))
+    ax2.plot(xs, ys, 'bx--', linewidth=2, markersize=5, label="Traffic")
+    ax2.legend(loc='upper right')
+    # plt.grid()
     plt.savefig(outfn, bbox_inches='tight')
     plt.close(fig)
 
@@ -130,6 +168,9 @@ def main():
         xlabel = "P%d MinRTT Relative Confidence Interval Size (CI/MinRTT)" % pct
         outfn = os.path.join(opts.outdir, 'ci_relsize_rtt%d.pdf' % pct)
         plot_ci_size_cdfs(limit2traffic, limit2relcdf, (0, 1), xlabel, outfn)
+
+        outfn = os.path.join(opts.outdir, 'ci_size_limits_rtt%d.pdf' % pct)
+        plot_limits(limit2traffic, limit2cdf, pct, outfn)
 
 
 if __name__ == '__main__':
