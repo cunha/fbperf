134f5779f56fe263820fd127d33f2937b83bad95
Italo Cunha <cunha@dcc.ufmg.br>
2019-05-13 04:22:55 +0000

    update prevalence plots

--- prevalence/plot.py
+++ prevalence/plot.py
@@ -22,12 +22,13 @@ def plot_cdfs(label2cdf, xlabel, outfn, **kwargs):
     os.makedirs(outdir, exist_ok=True)
     fig, ax1 = plt.subplots()
     ax1.set_xlabel(xlabel, fontsize=16)
-    if 'ylabel' in kwargs:
-        ax1.set_ylabel(kwargs['ylabel'], fontsize=16)
+    ax1.tick_params(axis="both", which="major", labelsize=14)
+    if "ylabel" in kwargs:
+        ax1.set_ylabel(kwargs["ylabel"], fontsize=16)
     else:
         ax1.set_ylabel("Cumulative Fraction Prefixes", fontsize=16)
-    if 'xlim' in kwargs:
-        ax1.set_xlim(kwargs['xlim'][0], kwargs['xlim'][1])
+    if "xlim" in kwargs:
+        ax1.set_xlim(kwargs["xlim"][0], kwargs["xlim"][1])
     else:
         ax1.set_xlim(0, 1)
     ax1.set_ylim(0, 1)
@@ -35,7 +36,7 @@ def plot_cdfs(label2cdf, xlabel, outfn, **kwargs):
     for label, cdf in label2cdf.items():
         xs, ys = zip(*cdf)
         ax1.plot(xs, ys, label=label)
-    plt.legend(loc="best")
+    plt.legend(loc="best", fontsize=14)
     plt.grid()
     plt.savefig(outfn, bbox_inches="tight")
     plt.close(fig)
@@ -53,6 +54,7 @@ def get_label2cdf_minrtt(base, filtr, fn):
     cdf = read_cdf(fp)
     label2cdf["Diff > 0ms"] = cdf
     return label2cdf
+
 
 def get_label2cdf_hdratio(base, filtr, fn):
     label2cdf = OrderedDict()
@@ -73,61 +75,79 @@ def main():
 
     for base in ["nonsticky", "sticky"]:
         for filtr in ["min_number_of_bins", "significant_improv_min_bins"]:
-            os.makedirs(os.path.join('output', base), exist_ok=True)
+            os.makedirs(os.path.join("output", base), exist_ok=True)
 
             fn = "frac-improved-bins.cdf"
             label2cdf = get_label2cdf_minrtt(base, filtr, fn)
-            outfn = os.path.join('output', base, 'minrtt', filtr, fn)
+            outfn = os.path.join("output", base, "minrtt", filtr, fn)
             outfn = outfn.replace(".cdf", ".pdf")
-            plot_cdfs(label2cdf, "Fraction of Bins Shifted", outfn)
+            plot_cdfs(label2cdf, "Fraction of Time with Improvement", outfn)
             label2cdf = get_label2cdf_hdratio(base, filtr, fn)
-            outfn = os.path.join('output', base, 'hdratio', filtr, fn)
+            outfn = os.path.join("output", base, "hdratio", filtr, fn)
             outfn = outfn.replace(".cdf", ".pdf")
-            plot_cdfs(label2cdf, "Fraction of Bins Shifted", outfn)
+            plot_cdfs(label2cdf, "Fraction of Time with Improvement", outfn)
 
             fn = "frac-improved-bins-weighted.cdf"
             label2cdf = get_label2cdf_minrtt(base, filtr, fn)
-            outfn = os.path.join('output', base, 'minrtt', filtr, fn)
+            outfn = os.path.join("output", base, "minrtt", filtr, fn)
             outfn = outfn.replace(".cdf", ".pdf")
-            plot_cdfs(label2cdf, "Fraction of Bins Shifted", outfn,
-                    ylabel="Cumulative Fraction of Traffic")
+            plot_cdfs(
+                label2cdf,
+                "Fraction of Time with Improvement",
+                outfn,
+                ylabel="Cumulative Fraction of Traffic",
+            )
             label2cdf = get_label2cdf_hdratio(base, filtr, fn)
-            outfn = os.path.join('output', base, 'hdratio', filtr, fn)
+            outfn = os.path.join("output", base, "hdratio", filtr, fn)
             outfn = outfn.replace(".cdf", ".pdf")
-            plot_cdfs(label2cdf, "Fraction of Bins Shifted", outfn,
-                    ylabel="Cumulative Fraction of Traffic")
+            plot_cdfs(
+                label2cdf,
+                "Fraction of Time with Improvement",
+                outfn,
+                ylabel="Cumulative Fraction of Traffic",
+            )
 
             fn = "frac-improved-bytes.cdf"
             label2cdf = get_label2cdf_minrtt(base, filtr, fn)
-            outfn = os.path.join('output', base, 'minrtt', filtr, fn)
+            outfn = os.path.join("output", base, "minrtt", filtr, fn)
             outfn = outfn.replace(".cdf", ".pdf")
             plot_cdfs(label2cdf, "Fraction of Traffic Shifted", outfn)
             label2cdf = get_label2cdf_hdratio(base, filtr, fn)
-            outfn = os.path.join('output', base, 'hdratio', filtr, fn)
+            outfn = os.path.join("output", base, "hdratio", filtr, fn)
             outfn = outfn.replace(".cdf", ".pdf")
             plot_cdfs(label2cdf, "Fraction of Traffic Shifted", outfn)
 
             fn = "num-shifts-per-day.cdf"
             label2cdf = get_label2cdf_minrtt(base, filtr, fn)
-            outfn = os.path.join('output', base, 'minrtt', filtr, fn)
+            outfn = os.path.join("output", base, "minrtt", filtr, fn)
             outfn = outfn.replace(".cdf", ".pdf")
-            plot_cdfs(label2cdf, "Number of Shifts/Day", outfn, xlim=(0,15))
+            plot_cdfs(label2cdf, "Number of Shifts/Day", outfn, xlim=(0, 15))
             label2cdf = get_label2cdf_hdratio(base, filtr, fn)
-            outfn = os.path.join('output', base, 'hdratio', filtr, fn)
+            outfn = os.path.join("output", base, "hdratio", filtr, fn)
             outfn = outfn.replace(".cdf", ".pdf")
-            plot_cdfs(label2cdf, "Number of Shifts/Day", outfn, xlim=(0,15))
+            plot_cdfs(label2cdf, "Number of Shifts/Day", outfn, xlim=(0, 15))
 
             fn = "num-shifts-per-day-weighted.cdf"
             label2cdf = get_label2cdf_minrtt(base, filtr, fn)
-            outfn = os.path.join('output', base, 'minrtt', filtr, fn)
+            outfn = os.path.join("output", base, "minrtt", filtr, fn)
             outfn = outfn.replace(".cdf", ".pdf")
-            plot_cdfs(label2cdf, "Number of Shifts/Day", outfn, xlim=(0,15),
-                    ylabel="Cumulative Fraction of Traffic")
+            plot_cdfs(
+                label2cdf,
+                "Number of Shifts/Day",
+                outfn,
+                xlim=(0, 15),
+                ylabel="Cumulative Fraction of Traffic",
+            )
             label2cdf = get_label2cdf_hdratio(base, filtr, fn)
-            outfn = os.path.join('output', base, 'hdratio', filtr, fn)
+            outfn = os.path.join("output", base, "hdratio", filtr, fn)
             outfn = outfn.replace(".cdf", ".pdf")
-            plot_cdfs(label2cdf, "Number of Shifts/Day", outfn, xlim=(0,15),
-                    ylabel="Cumulative Fraction of Traffic")
+            plot_cdfs(
+                label2cdf,
+                "Number of Shifts/Day",
+                outfn,
+                xlim=(0, 15),
+                ylabel="Cumulative Fraction of Traffic",
+            )
 
             # fn = "prev-per-prefix.cdf"
             # label2cdf = get_label2cdf_minrtt(base, filtr, fn)
