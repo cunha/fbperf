342d7eba8849592f1e9020482d22d9766cc0f464
Italo Cunha <cunha@dcc.ufmg.br>
2019-09-19 00:36:16 -0400

    update config

--- rust/src/bin/perfstats.rs
+++ rust/src/bin/perfstats.rs
@@ -28,35 +28,41 @@ struct Opt {
 }
 
 fn build_summarizers(db: &db::DB) -> Vec<Box<dyn TimeBinSummarizer>> {
-    let max_minrtt50_diff_ci_halfwidth: f32 = 25.0;
     let max_minrtt50_ci_halfwidth: u16 = 25;
-    let max_hdratio50_diff_ci_halfwidth: f32 = 0.20;
     let max_hdratio50_ci_halfwidth: f32 = 0.20;
     let max_hdratio_boot_diff_ci_fullwidth: f32 = 0.20;
     let mut summarizers: Vec<Box<dyn TimeBinSummarizer>> = Vec::new();
-    for &min_minrtt50_diff in [0, 5, 10, 20, 50].iter() {
-        let ml = Box::new(summarizers::opportunity::MinRtt50ImprovementSummarizer {
-            minrtt50_min_improv: min_minrtt50_diff,
-            max_minrtt50_diff_ci_halfwidth,
-            compare_lower_bound: true,
-        });
-        summarizers.push(ml);
-        let ml = Box::new(summarizers::degradation::MinRtt50LowerBoundDegradationSummarizer::new(
-            0.1,
-            min_minrtt50_diff,
-            max_minrtt50_diff_ci_halfwidth,
-            max_minrtt50_ci_halfwidth,
-            db,
-        ));
-        summarizers.push(ml);
+
+    for &max_minrtt50_diff_ci_halfwidth in [10.0f32, 20.0f32].iter() {
+        for &min_minrtt50_diff in [5, 10, 20].iter() {
+            let ml = Box::new(summarizers::opportunity::MinRtt50ImprovementSummarizer {
+                minrtt50_min_improv: min_minrtt50_diff,
+                max_minrtt50_diff_ci_halfwidth,
+                compare_lower_bound: true,
+            });
+            summarizers.push(ml);
+        }
+        for &min_minrtt50_diff in [5, 10, 20, 50].iter() {
+            let ml =
+                Box::new(summarizers::degradation::MinRtt50LowerBoundDegradationSummarizer::new(
+                    0.1,
+                    min_minrtt50_diff,
+                    max_minrtt50_diff_ci_halfwidth,
+                    max_minrtt50_ci_halfwidth,
+                    db,
+                ));
+            summarizers.push(ml);
+        }
     }
-    for &min_hdratio_diff in [0.0, 0.02, 0.05, 0.1, 0.2].iter() {
-        let hl = Box::new(summarizers::opportunity::HdRatio50ImprovementSummarizer {
-            hdratio50_min_improv: min_hdratio_diff,
-            max_hdratio50_diff_ci_halfwidth,
-            compare_lower_bound: true,
-        });
-        summarizers.push(hl);
+    for &min_hdratio_diff in [0.05, 0.1, 0.2].iter() {
+        for &max_hdratio50_diff_ci_halfwidth in [0.1f32, 0.2f32].iter() {
+            let hl = Box::new(summarizers::opportunity::HdRatio50ImprovementSummarizer {
+                hdratio50_min_improv: min_hdratio_diff,
+                max_hdratio50_diff_ci_halfwidth,
+                compare_lower_bound: true,
+            });
+            summarizers.push(hl);
+        }
         let hl =
             Box::new(summarizers::opportunity::HdRatioBootstrapDifferenceImprovementSummarizer {
                 hdratio_boot_min_improv: min_hdratio_diff,
@@ -64,14 +70,19 @@ fn build_summarizers(db: &db::DB) -> Vec<Box<dyn TimeBinSummarizer>> {
                 compare_lower_bound: true,
             });
         summarizers.push(hl);
-        let hl = Box::new(summarizers::degradation::HdRatio50LowerBoundDegradationSummarizer::new(
-            0.9,
-            min_hdratio_diff,
-            max_hdratio50_diff_ci_halfwidth,
-            max_hdratio50_ci_halfwidth,
-            db,
-        ));
-        summarizers.push(hl);
+    }
+    for &min_hdratio_diff in [0.05, 0.1, 0.2, 0.5, 0.75].iter() {
+        for &max_hdratio50_diff_ci_halfwidth in [0.1f32, 0.2f32].iter() {
+            let hl =
+                Box::new(summarizers::degradation::HdRatio50LowerBoundDegradationSummarizer::new(
+                    0.9,
+                    min_hdratio_diff,
+                    max_hdratio50_diff_ci_halfwidth,
+                    max_hdratio50_ci_halfwidth,
+                    db,
+                ));
+            summarizers.push(hl);
+        }
     }
     summarizers
 }
@@ -84,20 +95,9 @@ fn build_temporal_configs() -> Vec<perfstats::TemporalConfig> {
         min_frac_existing_bins: 0.6,
         min_frac_bins_with_alternate: 0.6,
         min_frac_valid_bins: 0.6,
-        continuous_min_frac_shifted_bins: 0.5,
-        diurnal_min_bad_bins: 1,
-        diurnal_bad_bin_min_prob_shift: 0.75,
-        uneventful_max_frac_shifted_bins: 0.01,
-    });
-    configs.push(perfstats::TemporalConfig {
-        bin_duration_secs: 900,
-        min_days: 2,
-        min_frac_existing_bins: 0.6,
-        min_frac_bins_with_alternate: 0.6,
-        min_frac_valid_bins: 0.6,
-        continuous_min_frac_shifted_bins: 0.5,
-        diurnal_min_bad_bins: 1,
-        diurnal_bad_bin_min_prob_shift: 0.75,
+        continuous_min_frac_shifted_bins: 0.75,
+        diurnal_min_bad_bins: 4,
+        diurnal_bad_bin_min_prob_shift: 0.5,
         uneventful_max_frac_shifted_bins: 0.0,
     });
     configs.push(perfstats::TemporalConfig {
@@ -108,18 +108,7 @@ fn build_temporal_configs() -> Vec<perfstats::TemporalConfig> {
         min_frac_valid_bins: 0.6,
         continuous_min_frac_shifted_bins: 0.75,
         diurnal_min_bad_bins: 1,
-        diurnal_bad_bin_min_prob_shift: 0.75,
-        uneventful_max_frac_shifted_bins: 0.01,
-    });
-    configs.push(perfstats::TemporalConfig {
-        bin_duration_secs: 900,
-        min_days: 2,
-        min_frac_existing_bins: 0.6,
-        min_frac_bins_with_alternate: 0.6,
-        min_frac_valid_bins: 0.6,
-        continuous_min_frac_shifted_bins: 0.75,
-        diurnal_min_bad_bins: 1,
-        diurnal_bad_bin_min_prob_shift: 0.75,
+        diurnal_bad_bin_min_prob_shift: 0.5,
         uneventful_max_frac_shifted_bins: 0.0,
     });
     configs
